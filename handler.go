481f2cebace48099e2590e0d0e8dc3805be97935
Masahiro Sano <sabottenda@gmail.com>
2016-06-26 17:30:11 +0900

    remove stream

--- handler.go
+++ handler.go
@@ -7,16 +7,9 @@ import (
 )
 
 var _ grpc.UnaryServerInterceptor = UnaryZapHandler
-var _ grpc.StreamServerInterceptor = StreamZapHandler
 
 func UnaryZapHandler(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-	logger.With(zap.String("method", info.FullMethod))
-	ctx = NewContext(ctx, l)
-	return handler(ctx, req)
-}
-
-func StreamZapHandler(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) (err error) {
 	l := logger.With(zap.String("method", info.FullMethod))
 	ctx = NewContext(ctx, l)
-	return handler(ctx, stream)
+	return handler(ctx, req)
 }
